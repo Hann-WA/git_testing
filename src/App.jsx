273d37074623f203a6054786669dae649d243b7a
MR. HAN WIN AUNG <66130500807@st.sit.kmutt.ac.th>
2025-04-18 19:41:17 +0700

    change website's name and text below

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,13 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>React Project Counter Website</h1>
+      <h1>Simple Counter Project</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          know your count!
         </p>
       </div>
       <p className="read-the-docs">
