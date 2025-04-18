7fd20993d324e6ef23dd0640469dd79244e34a65
Hann <hanwin.aung@kmutt.ac.th>
2025-04-18 19:49:33 +0700

    change website's name and text below

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Your simple counter for everyday!
         </p>
       </div>
       <p className="read-the-docs">
